8fa53582ae5b6baf9b80a3175098075c4cfeac43
“Martin <martin@forceandform.com>
2024-09-24 13:54:32 -0400

    Updated src/render.php to reflect copyright symbol and current year for front end users

--- src/render.php
+++ src/render.php
@@ -3,6 +3,4 @@
  * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
  */
 ?>
-<p <?php echo get_block_wrapper_attributes(); ?>>
-	<?php esc_html_e( 'Copyright Date Block – hello from a dynamic block!', 'copyright-date-block' ); ?>
-</p>
+<p <?php echo get_block_wrapper_attributes(); ?>>© <?php echo date( "Y" ); ?></p>
