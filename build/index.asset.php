650d823186be722ee3cf32b33cc3a50318130bd7
“Martin <martin@forceandform.com>
2024-09-24 14:06:23 -0400

    Updating build files to GitHub

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'cd49fa4552484208b8d5');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '017de8a5e8f95d47da05');
