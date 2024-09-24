88df5e6b792d7c7663cb3af7864e565c1de149a3
“Martin <martin@forceandform.com>
2024-09-24 14:04:04 -0400

    removed unecessary .scss files and view.js file

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'e99a49828c087a7397d8');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'cd49fa4552484208b8d5');
