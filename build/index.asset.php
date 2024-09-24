96c102cdbd60be495148d3bd2440df8b52fefaf9
“Martin <martin@forceandform.com>
2024-09-24 13:28:03 -0400

    added color and text controls to src/block.json

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '61cdd0dc5dba11d17c47');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '7144a73513a1778e20bd');
