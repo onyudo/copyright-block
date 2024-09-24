f6562a7cb34fb2c3f3b0c1df2f590a051d796297
“Martin <martin@forceandform.com>
2024-09-24 13:22:15 -0400

    Updated src/block.json

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '17cff5a9abcac9c52f43');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '61cdd0dc5dba11d17c47');
