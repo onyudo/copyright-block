a6fd4b26e6e75e5839c3ef6e11bdf340724b10bd
“Martin <martin@forceandform.com>
2024-09-26 15:09:06 -0400

    updated edit.js and block.json to accommodate for a start year

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '017de8a5e8f95d47da05');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'd370e8b7b2a3a9279ec5');
--- src/edit.js
+++ src/edit.js
@@ -11,7 +11,8 @@ import { __ } from '@wordpress/i18n';
  *
  * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-block-editor/#useblockprops
  */
-import { useBlockProps } from '@wordpress/block-editor';
+import { InspectorControls, useBlockProps } from '@wordpress/block-editor';
+import { PanelBody, TextControl, ToggleControl } from '@wordpress/components';
 
 /**
  * The edit function describes the structure of your block in the context of the
@@ -21,10 +22,49 @@ import { useBlockProps } from '@wordpress/block-editor';
  *
  * @return {Element} Element to render.
  */
-export default function Edit() {
+export default function Edit( { attributes, setAttributes } ) {
+	const { showStartingYear, startingYear } = attributes;
 	const currentYear = new Date().getFullYear().toString();
 
+	let displayDate;
+
+	if ( showStartingYear && startingYear ) {
+		displayDate = startingYear + '-' + currentYear;
+	} else {
+		displayDate = currentYear;
+	}
+
 	return (
-		<p { ...useBlockProps() }>© { currentYear }</p>
+		<>
+			<InspectorControls>
+				<PanelBody title={ __( 'Settings', 'copyright-date-block' ) }>
+					<ToggleControl
+						checked={ !! showStartingYear }
+						label={ __(
+							'Show starting year',
+							'copyright-date-block'
+						) }
+						onChange={ () =>
+							setAttributes ( {
+								showStartingYear: ! showStartingYear,
+							} )
+						}
+					/>
+					{ showStartingYear && (
+					<TextControl
+						label={ __(
+							'Starting year',
+							'copyright-date-block'
+						) }
+						value={ startingYear || '' }
+						onChange={ ( value ) =>
+							setAttributes( { startingYear: value } )
+						}
+					/>
+					) }
+				</PanelBody>
+			</InspectorControls>
+		<p { ...useBlockProps() }>© { displayDate }</p>
+		</>
 	);
 }
